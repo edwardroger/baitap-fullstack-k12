d2cb47ed4e662d50b78235a93e961f2bb6b17f13
Noha6789 <oanhdt39@gmail.com>
2023-05-09 19:32:02 +0700

    2

--- OANH/DAY15BTVN/js/scrift.js
+++ OANH/DAY15BTVN/js/scrift.js
@@ -6,24 +6,23 @@
 //Gợi ý: a + b > c và a + c > b và b + c > a
 //Tam giác cân / Tam giác đều / Tam giác vuông (Áp dụng định lý Pytago)
 
+
 var a = 5
-var b = 3
+var b = 4
 var c = 4
 
-if((a+b)>c && (b+c)>a && (c+a)>b) {
+if((a + b > c) && (b + c > a) && (c + a > b)) {
     document.write("a,b,c là 3 cạnh của 1 tam giác")
-} else if((a+b)<c||(a+c)<b||(b+c)<a){
+    if(a===b && b===c) {
+        document.write("tam giác đều")
+    } else if (a===b || a===c || b===c) {
+        document.write("tam giác cân")
+    } else if((a**2 + b**2 === c**2)||(a**2 + c**2 === b**2)||(b**2 + c**2 === a**2)) { 
+        document.write("tam giác vuông")
+    }
+} else {
     document.write("a,b,c không phải là ba cạnh của 1 tam giác")
-} 
+}
 
-//if((a+b)>c && (b+c)>a && (c+a)>b) {console.log('a,b,c là 3 cạnh của 1 tam giác');}
-//else if((a+b)<c){console.log('a,b,c không phải là ba cạnh của 1 tam giác');} 
-
-
-if(a===b&&b===c && (a+b)>c && (b+c)>a && (c+a)>b){
-    document.write("tam giác đều")
-} else if (a===b||a===c||b===c && (a+b)>c && (b+c)>a && (c+a)>b){
-    document.write("tam giác cân")
-} else if((a**a + b**b)===c**c||(a**a+c**c)===b**b||(b**b+c**c) === a**a ){document.write("tam giác vuông")}
 
 
