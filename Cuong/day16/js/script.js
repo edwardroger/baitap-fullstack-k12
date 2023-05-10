1b6bfc51ff7a72dd1aee4904c0576c5b9bcf2b36
nguyenmanhcuong754 <cuongncz754@gmail.com>
2023-05-10 20:12:33 +0700

    btvn

---
+++ Cuong/day16/js/script.js
@@ -0,0 +1,35 @@
+//for (let a = 0; a <= 1000; a++) {
+  //  console.log(a);  
+//}
+// nhập vào các số bất kì .In ra tất cả các số bé hơn a 
+//let a = parseInt(prompt("Nhập vào đây"));
+//for (let i = 0; i < a; i++) {
+ //   console.log(i);
+//}
+// nhap số a bất kỳ In các số chẵn lớn hơn 0 và bé hơn a
+//let a = parseInt(prompt("Nhập vào đây"));
+//for (let i = 0; i < a ; i++)
+//    if(i %2==0 )
+//    console.log(i);
+
+//BTVN 
+//In ra các số từ 1-1000 theo thứ tự tăng dần 
+//In ra các số từ 1-1000 theo thứ tự giảm dần 
+//nhập vào 1 số bất kì In ra bảng cửu chương số đó 
+
+
+//C1:
+//let a = parseInt(prompt("Nhập vào đây"));
+//for (let i = 0; i <= 1000; i++) {
+//    console.log(i);
+//}
+//C2
+//let a = parseInt(prompt("Nhập vào đây"));
+//for (let i = 1000; i > 0; i--) {
+//    
+//    console.log(i);
+//}
+//C3
+let a = parseInt(prompt("Nhập vào đây"));
+for (let i = 0; i < 11; i++) {
+    console.log(i + "X" + a + "=" + (i*a) );}
