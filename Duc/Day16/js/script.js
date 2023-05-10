2d14a9dfe609ecbaeb88726def67ba8a5d954945
Sammy25021997 <duc.hoangminh2502@gmail.com>
2023-05-10 19:23:47 +0700

    16th

---
+++ Duc/Day16/js/script.js
@@ -0,0 +1,37 @@
+
+
+// for (let a = 0; a < 10; a++) {
+//     console.log(a);
+// }
+
+//Nhập vào 1 số bất kì, In ra các số nhỏ hơn số đó
+
+// let x = prompt('Nhập vào x');
+
+// for (let y = 0; y < x; y++) {
+//     console.log(y);
+// }
+
+// Ví dụ 2: nhập vào số a bất kỳ. In ra các số chẵn lớn hơn 0 và <a
+
+// let a = prompt('Ê bấm bừa đê tao cho xem cái này!')
+
+// for (let x = 0; x < a; x++) {
+//     if (x % 2 == 0) {
+//         console.log(x);
+//     }
+// }
+
+//BTVN: 
+//Câu 1: In ra từ 1-1000
+//Câu 2: In ra từ 1000-1
+//Câu 3: Nhập vào 1 số bất kỳ > 0. In ra bảng cửu chương của số đó
+
+//Câu 3:
+
+let a = prompt('Ê bấm bừa đê tao cho xem cái này!')
+
+for(let x = 1; x <= 10; x++) {
+    z = x*a;
+    console.log(x + "x" + a + "=" + z);
+}
