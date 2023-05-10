c17e45945bd320c439b7de5a999ee7ef8de71bae
ngngkhang <khangbeo306@gmail.com>
2023-05-10 19:30:52 +0700

    btjs

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+let n = parseInt(prompt("nhap vao"));
+// for ( let a=0; a < n; a++ ) { 
+//     console.log( a );
+// }
+
+for ( let i=0; i<n; i++ ) {
+    if ( i %2 == 0 ) {
+        {
+            console.log( i);
+        }
+    }
+}
+
+
+for ( let i=1000; i>=0; i-- ) {
+    console.log( i );
+}
+
+
+for ( let i=0; i<11; i++ ) {
+    console.log(i+" x "+n+" = "+i*n);
+}
