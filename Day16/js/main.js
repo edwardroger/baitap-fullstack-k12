66769b26e6e6df463da93c69dfa170927e462bcc
Ngocchim-12345 <anninhthaiduong@gmail.com>
2023-05-10 19:04:58 +0700

    day16

---
+++ Day16/js/main.js
@@ -0,0 +1,48 @@
+// for (let a = 0; a < 10; a++){
+//     console.log(a)
+// }
+
+//Lần 1: a = 0; a < 10 => log(0); a++ => 0 + 1
+//Lần 2: a = 1; a < 10 => log(1); a++ => 1 + 1
+//Lần 3: a = 2; a < 10 => log(2); a++ => 2 + 1
+//..
+//Lần 10: a = 9; 9 < 10 => log(9); a++ => 9 + 1
+//Lần 11: a = 10; 10 = 10 => log(10); => false
+
+
+
+// var a = parseInt(prompt('nhập vào số a'));
+
+// for (let i = 0; i < a; i++){
+//     console.log(i);
+// }
+
+
+// for (let i = 0; i < a && i % 2 === 0; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+    
+// }
+
+// for(let i = 0; i < a; i++){
+//   if(i > 0){
+//     console.log(i);
+//   }
+// }
+
+
+// BTVN
+
+//Tăng dần
+
+for (let i = 1; i <= 1000; i++) {
+  console.log(i);
+}
+
+// Giảm dần
+
+for (let i = 1000; i >= 1; i--) {
+  console.log(i);
+}
+
