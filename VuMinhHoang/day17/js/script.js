d1216903bd96f1f760cffe28a9ae3f2ecf93923f
Hoangvu1520 <danteerck1520@gmail.com>
2023-05-12 19:21:00 +0700

    update

---
+++ VuMinhHoang/day17/js/script.js
@@ -0,0 +1,108 @@
+let a = parseInt(prompt("Nhập a: "));
+let b = parseInt(prompt("Nhập b: "));
+let c = parseInt(prompt("Nhập c: "));
+
+//Bai 1
+// let s = 0;
+// for (let i = 0; i <= a; i++) {
+//     if (i % 2 !== 0) {
+//         s = s + i
+//     }
+// }
+// console.log(s)
+
+//Bai 2
+// let s = 0;
+// let c = 0;
+// for (let i = 0; i <= a; i++) {
+//     if (i % 5 == 0) {
+//         s = s + i;
+//         c++;
+//     }
+// }
+// console.log(s/c);
+
+//Bai 3
+// t = 1
+// if (a > 0) {
+//     for (let i = a; i > 0; i--) {
+//         t = t * i
+//     }
+// }
+// else if(a=0){
+//     t=1
+// }
+// console.log(t);
+
+//Bai 4
+// let s = 0;
+// for (let i = 1; i <= a; i++) {
+//     s = s + (i * i)
+// }
+// console.log(s)
+
+//Bai 5
+// let c=0;
+// for(i=0;i<=a;i++){
+//     if(i%2!==0){
+//         c++;
+//     }
+// }
+// console.log(c)
+
+//Bai 6
+// let s = 0;
+// if (a < b) {
+//     for (let i = a; i <= b; i++) {
+//         s = s + i;
+//     }
+// }
+// else if (a === b) {
+//     s = a + b;
+// }
+// else if (a > b) {
+//     for (let i = b; i <= a; i++) {
+//         s = s + i;
+//     }
+// }
+// console.log(s)
+
+//Bai 9
+// let ar = [];
+// for (let i = 0; i < 5; i++) {
+//     ar.push(parseInt(prompt("nhập phần tử thứ " + (i + 1))));
+// };
+// for (let i = 0; i < 5; i++) {
+//     if (ar[i] % 2 == 0) {
+//         console.log(ar[i])
+//     }
+// };
+
+//Bai 10
+// let ar = [];
+// let s = 0;
+// for (let i = 0; i < 5; i++) {
+//     ar.push(parseInt(prompt("nhập phần tử thứ " + (i + 1))));
+// };
+// for (let i = 0; i < 5; i++) {
+//     s = s + ar[i]
+// };
+// console.log(s / 5);
+
+//Bai 7
+// let d = b * b - 4 * a * c;
+// let x1;
+// let x2;
+// if (d > 0) {
+//     x1 = (-b + Math.sqrt(d)) / (2 * a);
+//     x2 = (-b - Math.sqrt(d)) / (2 * a);
+//     console.log("Nghiệm của phương trình là: " + x1 + "," + x2)
+// }
+
+// else if (d = 0) {
+//     x1 = (-b) / (2 * a)
+//     console.log("Phương trình có nghiệm kép:" + x1);
+// }
+// else { console.log("Phương trình vô nghiệm") }
+
+//Bai 8
