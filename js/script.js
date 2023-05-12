d1bd2fdda1c490bb16b7a7bf70abdf063cf23c96
ngngkhang <khangbeo306@gmail.com>
2023-05-12 19:43:14 +0700

    buoi 17 lan 2

--- js/script.js
+++ js/script.js
@@ -23,91 +23,104 @@
 // }
 
 
-let a = parseInt(prompt("nhap vao a"));
-let b = parseInt(prompt("nhap vao b"));
-let c = parseInt(prompt("nhap vao c"));
-let s1 = 0;
+let a = parseFloat(prompt("nhap vao a"));
+let b = parseFloat(prompt("nhap vao b"));
+let c = parseFloat(prompt("nhap vao c"));
 
-for ( let i = 0; i<=a; i++ ) {
-    if ( i %2 !== 0 ) {
-        s1 += i;
+if ( a < 0  && b < 0 && c < 0)
+console.log(" sai ");
+else
+{
+
+    let s1 = 0;
+    for ( let i = 0; i<=a; i++ ) {
+        if ( i %2 !== 0 ) {
+            s1 += i;
+        }
     }
-}
-console.log( 'tong cac so le tu o den ' + a + ' la ' + s1)
+    console.log( 'tong cac so le tu o den ' + a + ' la ' + s1)
 
-let s2 = 0;
-for ( let i = 0; i<=a; i++ ) {
-    if ( i %5 === 0 ) {
-        s2 += i;
+
+    let s2 = 0;
+    for ( let i = 0; i<=a; i++ ) {
+        if ( i %5 === 0 ) {
+            s2 += i;
+        }
     }
-}
-console.log('trung binh cong cac so chia het ho 5 tu 0 den ' + a + ' la ' + s2)
+    console.log('trung binh cong cac so chia het ho 5 tu 0 den ' + a + ' la ' + s2)
 
-let s3 = 1;
-for ( let i = 1; i<=a; i++ ) {
-    s3 = s3*i;
-}
-console.log('giai thua cua ' + a + ' la ' + s3)
 
-let s4 = 0;
-for ( let i = 0; i<=a; i++ ) {
-    s4 += (i+1)**2;
-}
-console.log('tong binh phuong cua ' + a + ' la ' + s4)
-
-let s5 = 0;
-for ( let i = 1; i<=a; i++ ) {
-    if (i % 2 !== 0) {
-        s5 = s5 + i;
+    let s3 = 1;
+    for ( let i = 1; i<=a; i++ ) {
+        s3 = s3*i;
     }
-}
- console.log('so cac so le tu 0 den ' + a + ' la ' + s5)
+    console.log('giai thua cua ' + a + ' la ' + s3)
 
-let s6 = a;
-if ( a - b >= 0 ) {
-    for ( let i = b; i <= a; i++ ) {
-        s6 = s6 + i;
+
+    let s4 = 0;
+    for ( let i = 0; i<=a; i++ ) {
+        s4 += (i+1)**2;
     }
-    console.log('tong tu ' + b + ' toi ' + a + ' la ' + s6)}
-    else {
-        for ( let i = a; i <= b; i++ ) {
+    console.log('tong binh phuong cua ' + a + ' la ' + s4)
+
+
+    let s5 = 0;
+    for ( let i = 1; i<=a; i++ ) {
+        if (i % 2 !== 0) {
+            s5 = s5 + i;
+        }
+    }
+    console.log('so cac so le tu 0 den ' + a + ' la ' + s5)
+
+
+    let s6 = a;
+    if ( a - b >= 0 ) {
+        for ( let i = b; i <= a; i++ ) {
             s6 = s6 + i;
         }
-    console.log('tong tu ' + a + ' toi ' + b + ' la ' + s6)
+        console.log('tong tu ' + b + ' toi ' + a + ' la ' + s6)}
+        else {
+            for ( let i = a; i <= b; i++ ) {
+                s6 = s6 + i;
+            }
+        console.log('tong tu ' + a + ' toi ' + b + ' la ' + s6)
+        }
+
+
+    let s9 = []
+    let s10 = 0
+    for ( let i = 0; i<5; i++) {
+        s9[i] = parseInt(prompt('nhap vao phan tu thu ' + i ))
     }
 
-
-let s9 = []
-let s10 = 0
-for ( let i = 0; i<5; i++) {
-    s9[i] = parseInt(prompt('nhap vao phan tu thu ' + i ))
-}
-console.log(' cac so chan trong mang la ');
-for ( let i = 0; i<5; i++) {
-    if ( s9[i] % 2 === 0) {
-        console.log(s9[i]);
+    console.log(' cac so chan trong mang la ');
+    for ( let i = 0; i<5; i++) {
+        if ( s9[i] % 2 === 0) {
+            console.log(s9[i]);
+        }
     }
-}
-console.log('trung binh cong cua mang la ')
-for ( let i = 0; i<5; i++){
-    s10 += s9[i];
-}
-console.log(s10/5)
 
-
-console.log('phuong trinh ' + a + 'x^2 + ' + b + 'x + ' + c )
-let delta = b*b - 4*a*c;
-let x1 = 0;
-let x2 = 0;
-if ( a != 0) {
-    if ( delta < 0 ) {
-        console.log(' la mot phuong trinh vo nghiem')
-    }else
-    if ( delta = 0) {
-        console.log('la mot phuong trinh co 1 nghiem duy nhat ' + (-b)/(2*a))
-    }else
-    if ( delta > 0) {
-        console.log('la mot phuong trinh co 2 nghiem phan biet ' + (-b+sqrt(delta))/(2*a) + ' va ' + (-b-sqrt(delta))/(2*a))
+    console.log('trung binh cong cua mang la ')
+    for ( let i = 0; i<5; i++){
+        s10 += s9[i];
     }
+    console.log(s10/5)
+
+
+    console.log('phuong trinh ' + a + 'x^2 + ' + b + 'x + ' + c )
+    let delta = b*b - 4*a*c;
+    let delta2 = Math.sqrt( delta);
+    if ( a != 0) {
+        if ( delta < 0 ) {
+            console.log(' la mot phuong trinh vo nghiem')
+        }else
+        if ( delta = 0) {
+            console.log('la mot phuong trinh co 1 nghiem duy nhat ' + [(-b)/(2*a)])
+        }else
+        if ( delta > 0) {
+            console.log('la mot phuong trinh co 2 nghiem phan biet ' + [(-b+Math.sqrt(delta))/(2*a)] + ' va ' + [(-b-Math.sqrt(delta))/(2*a)])
+        }
+    }
+    else (console.log('phuong trinh vo nghiem'));
+        
 }
-else (console.log('phuong trinh vo nghiem'));
