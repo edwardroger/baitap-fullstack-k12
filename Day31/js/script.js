af608f942e9ab52e4c86ee6371c779876b108c37
Ngocchim-12345 <anninhthaiduong@gmail.com>
2023-06-19 18:23:56 +0700

    day31

---
+++ Day31/js/script.js
@@ -0,0 +1,34 @@
+// 1. khai báo biến
+// 2. câu điều kiện if else
+// 3. vòng lặp for
+
+// 4. kiểu dữ liệu
+// 4.1 các kiểu dữ liệu
+// - Number (kiểu số)
+// - string (chuỗi kí tự)
+// - Array (mảng)
+// - object (đối tượng)
+// - Boolean (kiểu đúng : sai | true : false Ư 1 : 0)
+// - undefined (kiểu dữ liệu lỗi: chưa đc định nghĩa)
+// - Null (dữ liệu rỗng)
+// - NaN (not a number)
+
+
+// 5. Operator
+//Các kiểu operator:
+// !== (khác. ví dụ so sánh a !== b)
+// = (phép gán giá trị a = b)
+// == (so sánh 2 giá trị VD: a = 5, b = '5', a == b return true)
+// === (so sánh 2 giá trị và kiểu dữ liệu của nó, a === b return false)
+// % chia hết Vd: a % 2 == 0
+// && Toán tử logic "Và". VD: condition = (a === b && b === c)
+// || Toán tử logic "HOẶC". VD: condition = (a === b || b === c)
+// ...
+
+//--------
+// toán tử 3 ngôi
+var a = 10;
+var b = 5;
+// var sum = (a > b) ? (a + b) : (a - b)
+
+//6. function
