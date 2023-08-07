ce2354371e8f54aeb16754ae9dbc1db827e3587e
Hoangvu1520 <danteerck1520@gmail.com>
2023-08-07 15:41:59 +0700

    update

--- VuMinhHoang/day34/project/src/App.jsx
+++ VuMinhHoang/day34/project/src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useCallback, useState } from "react";
-import InputNumber from "./day39/InputNumber";
-import Button from "./day39/Button";
-
+import { BrowserRouter as Routes, Route, Link, BrowserRouter } from "react-router-dom"
+import HomePage from "./day43/HomePage";
+import Login from "./day43/Login";
 
 const App = () => {
-    return (<>
-
-    </>)
+    return (
+            <Routes>
+                <Route path={'/'} element={HomePage} />
+                <Route path={'/login'} element={Login} />
+            </Routes>
+    )
 }
-export default App
+export default App 
